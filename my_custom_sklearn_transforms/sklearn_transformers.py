c8659fea54bc0738bc0ccd4ccfcc4a354d9642bd
Monique Ferreira <61131318+ferreiramonique@users.noreply.github.com>
2020-08-20 15:19:26 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -30,3 +30,19 @@ class ChangeGO(BaseEstimator, TransformerMixin):
         data.apply(lambda row: row['NOTA_DE'] if np.isnan(row['NOTA_GO']) else row['NOTA_GO'], axis=1)
         #Retornamos o dataframe:
         return data
+
+       
+class Normalizar(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        df_num = data.select_dtypes(include=[np.number])
+        df_norm = (df_num - df_num.min()) / (df_num.max() - df_num.min())
+        data[df_norm.columns] = df_norm
+        return data
